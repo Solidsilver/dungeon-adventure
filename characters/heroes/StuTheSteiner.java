773691b4d3aed41b783cfb7edd3c8683ffab161b
dnisyndrum@yahoo.com <dnisyndrum@yahoo.com>
2018-12-05 17:40:12 -0800

    gave stuthesteiner different attack

--- characters/heroes/StuTheSteiner.java
+++ characters/heroes/StuTheSteiner.java
@@ -14,15 +14,13 @@ public class StuTheSteiner extends Hero{
 	private static final int StuDamageMax = 20;
 	private static final double StuChanceToBlock = 0.1;
 	
-	String specialAttack = "Increase Hit Points";
-	
 	private final int MIN_ADD = 40;
 	private final int MAX_ADD = 70;
 	
 	public StuTheSteiner()
 	{
 		super(StuName, StuHitPoints, StuAttackSpeed, StuChanceToHit, StuDamageMin,
-				StuDamageMax, StuChanceToBlock, "Increase Hit Points");
+				StuDamageMax, StuChanceToBlock, "Surprise Attack");
     }//end constructor
 	
 	public int getMinAdd()
